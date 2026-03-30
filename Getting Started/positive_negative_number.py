187fe7ad28798cec541f0b295b79a8f9c9e47710
amarjit kumar <amarjit.kumar.11032001@gmail.com>
2026-03-30 23:25:46 +0530

    first code

--- Getting Started/positive_negative_number.py
+++ Getting Started/positive_negative_number.py
@@ -1,9 +1,7 @@
 num=int(input("enter the number"))
 if num>0:
 	print(num,"is a positive number")
-	if num==0:
-		print(num,"is zero")
-	else:
-		print("")
+elif num==0:
+	print(num,"is zero")
 else:
 	(num,"is negative number")
