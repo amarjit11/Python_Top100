a51c474318d0c7415105794848074e61c5bee280
amarjit kumar <amarjit.kumar.11032001@gmail.com>
2026-03-31 23:59:05 +0530

    odd even

--- Getting Started/even_odd.py
+++ Getting Started/even_odd.py
@@ -1,6 +1,10 @@
-num=int(input())
+'''num=int(input())
 
 if num%2==0:
 	print(num," is an even number")
 else:
 	print(num," is an odd number")
+	'''
+
+num=int(input())
+print("Even") if num%2 == 0 else print("Odd")
